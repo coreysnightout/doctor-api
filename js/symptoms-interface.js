05c0792368d140fe8b0cfce2255b97234173366d
corey redding <coreysnightout@gmail.com>
2017-06-30 16:44:43 -0700

    add some more js and console logs to try to post object into the index.html with no luck yet..

--- js/symptoms-interface.js
+++ js/symptoms-interface.js
@@ -3,13 +3,12 @@ var Doctors = require('./../js/symptoms.js').doctorModule;
 
 
 $(document).ready(function() {
-
   $('.searchButton').click(function() {
     var medicalIssue = $(".symptoms").val();
     var docObject = new Doctors();
     var searchResult = docObject.getDoctors(medicalIssue);
     // console.log(searchResult);
-    // console.log(Doctors.result);
+    console.log(searchResult);
     $('.output').append(searchResult);
   });
 });
--- js/symptoms.js
+++ js/symptoms.js
@@ -5,9 +5,9 @@ Doctors = function(){
 Doctors.prototype.getDoctors = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
   .then(function(result) {
-      return result.data[0];
-      $('.output').text(test);
-      console.log(test);
+    console.log(result);
+      var results = $(result.data[0]);
+      return results;
     })
    .fail(function(error){
       console.log("fail");
@@ -16,34 +16,5 @@ Doctors.prototype.getDoctors = function(medicalIssue) {
 
 exports.doctorModule = Doctors;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //business logic stuff for a particular module
-// var ExampleModule = function(args) {
-//   this.args = args; //to be replaced with constructor arguments
-// };
-//
-// ExampleModule.prototype.examplePrototype = function() {
-//   return `this is an example prototype method`;
-// };
-//
-// exports.exampleModule = ExampleModule;
+// return medicalIssue;
+// $('.output').text(test);
