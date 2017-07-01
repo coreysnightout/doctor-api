2585ac0cad4f169e4cecca6307186354e034ec3d
corey redding <coreysnightout@gmail.com>
2017-06-30 19:06:47 -0700

    add styles to index.html

--- js/symptoms-interface.js
+++ js/symptoms-interface.js
@@ -4,10 +4,10 @@ var Doctors = require('./../js/symptoms.js').doctorModule;
 
 $(document).ready(function() {
   $('.searchButton').click(function() {
+
     var medicalIssue = $(".symptoms").val();
     var docObject = new Doctors();
     var searchResult = docObject.getDoctors(medicalIssue);
-    // console.log(searchResult);
     console.log(searchResult);
     $('.output').append(searchResult);
   });
