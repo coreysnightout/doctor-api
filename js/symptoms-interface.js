4e04e8e0aa4e28ab76ed80aa01a97af3fc3f6af8
corey redding <coreysnightout@gmail.com>
2017-07-09 20:47:45 -0700

    attempt to push results in interface js

--- js/symptoms-interface.js
+++ js/symptoms-interface.js
@@ -1,6 +1,19 @@
 
 var Doctors = require('./../js/symptoms.js').doctorModule;
 
+var displayList = function(response) {
+  console.log(response);
+  for(i=0; i<10; i++)
+  {
+    var specialty = "specialty: " + response.data[i].specialties[0].name;
+    var location = "location: " + response.data[i].practices[0].location_slug;
+   $('.output').append("<li>" + "<strong>" +  response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</strong>" + "<br>" + "<img class='photo-" + i + "' src='" + response.data[i].profile.image_url + "'></img>" + "<br>" + location + "<br>" + specialty + "</li>");
+  }
+}
+
+
+console.log(displayList);
+
 $(document).ready(function() {
   $('.searchButton').click(function() {
 
--- js/symptoms.js
+++ js/symptoms.js
@@ -8,15 +8,14 @@ Doctors.prototype.getDoctors = function(medicalIssue) {
   var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey;
   console.log(medicalIssue);
 
-  $.get(resource_url, function (response) {
-     console.log(response);
-     for(i=0; i<10; i++)
-     {
-       var specialty = "specialty: " + response.data[i].specialties[0].name;
-       var location = "location: " + response.data[i].practices[0].location_slug;
-       console.log(specialty);
-      $('.output').append("<li>" + "<strong>" +  response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</strong>" + "<br>" + "<img class='photo-" + i + "' src='" + response.data[i].profile.image_url + "'></img>" + "<br>" + location + "<br>" + specialty + "</li>");
-     }
+  $.get(resource_url, function (displayList) {
+    //  console.log(response);
+    //  for(i=0; i<10; i++)
+    //  {
+    //    var specialty = "specialty: " + response.data[i].specialties[0].name;
+    //    var location = "location: " + response.data[i].practices[0].location_slug;
+    //   $('.output').append("<li>" + "<strong>" +  response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</strong>" + "<br>" + "<img class='photo-" + i + "' src='" + response.data[i].profile.image_url + "'></img>" + "<br>" + location + "<br>" + specialty + "</li>");
+    //  }
   });
 };
 
@@ -26,6 +25,18 @@ exports.doctorModule = Doctors;
 
 
 
+// $.get(resource_url, function (response) {
+//    console.log(response);
+//    for(i=0; i<10; i++)
+//    {
+//      var specialty = "specialty: " + response.data[i].specialties[0].name;
+//      var location = "location: " + response.data[i].practices[0].location_slug;
+//      console.log(specialty);
+//     $('.output').append("<li>" + "<strong>" +  response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "</strong>" + "<br>" + "<img class='photo-" + i + "' src='" + response.data[i].profile.image_url + "'></img>" + "<br>" + location + "<br>" + specialty + "</li>");
+//    }
+// });
+// };
+//
 
 
 
