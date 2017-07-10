c244c4af50cf22f01add348546d55f3a99564ed2
corey redding <coreysnightout@gmail.com>
2017-07-09 21:56:01 -0700

    add search results to append in the user interface logic

--- js/symptoms.js
+++ js/symptoms.js
@@ -8,7 +8,7 @@ Doctors.prototype.getDoctors = function(medicalIssue, displayList) {
   var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey;
   console.log(medicalIssue);
 
-  $.get(resource_url, function (response, displayList) {
+  $.get(resource_url, function (response) {
     displayList(response);
   });
 };
